f07c11869f326db65cd267220686e763d30cbd3d
heakknight <heakmeng42@gmail.com>
2026-03-12 21:36:00 +0700

    Delete svj icon

--- src/app/components/SidebarComponent.jsx
+++ src/app/components/SidebarComponent.jsx
@@ -5,11 +5,11 @@ const SidebarComponent = () => {
     <aside className='bg-[#11161D] w-1/6 h-full rounded-t-2xl p-4 text-white cursor-pointer ml-4'>
       <span className='text-sm'>Menu</span>
       <div className='flex flex-col gap-2'>
-        <p className='bg-[#1C2128] p-2 mt-5 rounded-lg'>Overview</p>
-        <p className='bg-[#1C2128] p-2 rounded-lg'>Items</p>
-        <p className='bg-[#1C2128] p-2 rounded-lg'>Orders</p>
-        <p className='bg-[#1C2128] p-2 rounded-lg'>Customers</p>
-        <p className='bg-[#1C2128] p-2 rounded-lg'>Settings</p>
+        <a className='bg-[#1C2128] p-2 mt-5 rounded-lg'>Overview</a>
+        <a className='bg-[#1C2128] p-2 rounded-lg'>Items</a>
+        <a className='bg-[#1C2128] p-2 rounded-lg'>Orders</a>
+        <a className='bg-[#1C2128] p-2 rounded-lg'>Customers</a>
+        <a className='bg-[#1C2128] p-2 rounded-lg'>Settings</a>
       </div>
     </aside>
   )
